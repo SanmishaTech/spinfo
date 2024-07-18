f381c1fbf8aedfa0375b2c5d028ad1c8f79c0de7
yashchaud <yashchaudhari908@gmail.com>
2024-07-18 19:54:14 +0530

    asd

--- resources/js/src/components/Layouts/Header.tsx
+++ resources/js/src/components/Layouts/Header.tsx
@@ -168,7 +168,7 @@ const Header = () => {
                     <div className="horizontal-logo flex lg:hidden justify-between items-center ltr:mr-2 rtl:ml-2">
                         <Link to="/" className="main-logo flex items-center shrink-0">
                             <img className="w-8 ltr:-ml-1 rtl:-mr-1 inline" src={Logo} alt="logo" />
-                            <span className="text-2xl ltr:ml-1.5 rtl:mr-1.5  font-semibold  align-middle hidden md:inline dark:text-white-light transition-all duration-300">SP Info</span>
+                            <span className="text-2xl ltr:ml-1.5 rtl:mr-1.5  font-semibold  align-middle hidden md:inline dark:text-white-light transition-all duration-300">SP INFO</span>
                         </Link>
                         <button
                             type="button"
